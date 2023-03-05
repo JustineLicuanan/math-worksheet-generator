9c7d66770bd2fcad4aba562eb54499ef7901845f
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-03-05 20:34:56 +0800

    setup dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
+  <div
+    class="h-screen p-5 transition-colors duration-75 dark:bg-gray-900 dark:text-gray-200"
+  >
+    <ThemeSwitch />
     <h1 class="text-center text-3xl">Hello world!</h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import ThemeSwitch from "./components/ThemeSwitch.vue";
+</script>
 
 <style scoped></style>
---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button
+      @click="toggleDark()"
+      class="rounded bg-green-500 py-1 px-4 text-white transition-colors duration-75 hover:bg-green-700"
+    >
+      <MoonIcon class="inline-block h-5 w-5 align-middle" v-if="isDark" />
+      <SunIcon class="inline-block h-5 w-5 align-middle" v-else />
+      <span class="ml-2">{{ isDark ? "Dark" : "Light" }}</span>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { MoonIcon, SunIcon } from "@heroicons/vue/24/outline";
+  import { useDark, useToggle } from "@vueuse/core";
+
+  const isDark = useDark();
+  const toggleDark = useToggle(isDark);
+</script>
+
+<style scoped></style>
