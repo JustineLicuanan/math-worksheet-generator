158013d6c418ea5755ae5867b81d9542b2b2e92e
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-03-06 18:10:16 +0800

    integrate localstorage

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,39 @@
 </template>
 
 <script setup lang="ts">
+  import { onMounted, watch } from "vue";
   import MainSection from "./components/MainSection.vue";
   import NavBar from "./components/NavBar.vue";
+  import { useWorksheet } from "./composables/worksheet";
+
+  const worksheetStore = useWorksheet();
+
+  onMounted(() => {
+    if ("currentOperation" in localStorage && "worksheets" in localStorage) {
+      worksheetStore.worksheets.value = JSON.parse(localStorage.worksheets);
+      worksheetStore.currentOperation.value = JSON.parse(
+        localStorage.currentOperation
+      );
+    }
+  });
+
+  watch(worksheetStore.currentOperation, () => {
+    localStorage.setItem(
+      "currentOperation",
+      JSON.stringify(worksheetStore.currentOperation.value)
+    );
+  });
+
+  watch(
+    worksheetStore.worksheets,
+    () => {
+      localStorage.setItem(
+        "worksheets",
+        JSON.stringify(worksheetStore.worksheets.value)
+      );
+    },
+    { deep: true }
+  );
 </script>
 
 <style scoped></style>
